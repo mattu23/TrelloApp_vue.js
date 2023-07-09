fe6b099579271432d4c9f976708ed207587f466a
mattu23 <hirokiim.27@gmail.com>
2023-07-09 22:25:59 +0900

    Todoアプリ完成版

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <board/>
+  </div>
+</template>
+
+<script>
+import Board from './components/Board.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Board
+  }
+}
+</script>
+
+
